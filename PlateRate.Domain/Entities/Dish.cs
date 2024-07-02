9f3233661df389de352f51140422429098981b40
Sinan <sinan.cakal00@gmail.com>
2024-07-02 19:59:46 +0300

    Db connection and init migration

--- PlateRate.Domain/Entities/Dish.cs
+++ PlateRate.Domain/Entities/Dish.cs
@@ -3,7 +3,9 @@
 public class Dish
 { 
     public int Id { get; set; }
+    public int RestaurantId { get; set; }
     public string Name { get; set; } = default!;
     public string Description { get; set; } = default!;
     public decimal Price { get; set; }
+    
 }
---
+++ PlateRate.Infrastructure/Persistence/PlateRateDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlateRate.Domain.Entities;
+
+namespace PlateRate.Infrastructure.Persistence;
+
+internal class PlateRateDbContext : DbContext
+{
+    internal DbSet<Restaurant> Restaurants { get; set; }
+    internal DbSet<Dish> Dishes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=sinan;Database=PlateRate;Integrated Security=True;TrustServerCertificate=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Restaurant>()
+            .OwnsOne(e => e.Address);
+        modelBuilder.Entity<Restaurant>()
+            .HasMany(e => e.Dishes)
+            .WithOne()
+            .HasForeignKey(d => d.RestaurantId);
+
+    }
+}
