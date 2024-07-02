9be0069e34ac9d9e5afe3399e6250a96d28d2011
Sinan <sinan.cakal00@gmail.com>
2024-07-02 19:05:56 +0300

    Created Entities

---
+++ PlateRate.Domain/Entities/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace PlateRate.Domain.Entities;
+
+public class Address
+{
+    public string? City { get; set; }
+    public string? Street { get; set; }
+    public string? PostalCode { get; set; }
+
+}
