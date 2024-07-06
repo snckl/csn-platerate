c02db1dd662b47fa7c04f71b9c606951e1f0c4cf
Sinan <sinan.cakal00@gmail.com>
2024-07-06 23:23:04 +0300

    Extended claims for user

---
+++ PlateRate.Infrastructure/Authorization/RestaurantUserClaimsPrincipleFactor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using PlateRate.Domain.Entities;
+using System.Security.Claims;
+
+namespace PlateRate.Infrastructure.Authorization;
+public class RestaurantUserClaimsPrincipleFactor(UserManager<User> userManager, RoleManager<IdentityRole> roleManager
+    ,IOptions<IdentityOptions> options) : UserClaimsPrincipalFactory<User, IdentityRole>(userManager, roleManager, options)
+{
+    public override async Task<ClaimsPrincipal> CreateAsync(User user)
+    {
+        var id = await GenerateClaimsAsync(user);
+
+        if (user.Nationality is not null) 
+        {
+            id.AddClaim(new Claim("Nationality",user.Nationality));
+        }
+
+        if(user.DateOfBirth is not null)
+        {
+            id.AddClaim(new Claim("DateOfBirth", user.DateOfBirth.Value.ToString("yyyy-MM-dd")!));
+        }
+
+        return new ClaimsPrincipal(id);
+    }
+}
--- PlateRate.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ PlateRate.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PlateRate.Domain.Entities;
 using PlateRate.Domain.Repositories;
+using PlateRate.Infrastructure.Authorization;
 using PlateRate.Infrastructure.Persistence;
 using PlateRate.Infrastructure.Repositories;
 using PlateRate.Infrastructure.Seeders;
@@ -17,6 +18,7 @@ public static class ServiceCollectionExtensions
         
         services.AddIdentityApiEndpoints<User>()
             .AddRoles<IdentityRole>()
+            .AddClaimsPrincipalFactory<RestaurantUserClaimsPrincipleFactor>()
             .AddEntityFrameworkStores<PlateRateDbContext>();
         services.AddScoped<IRestaurantSeeder, RestaurantSeeder>();
         services.AddScoped<IRestaurantRepository, RestaurantsRepository>();
