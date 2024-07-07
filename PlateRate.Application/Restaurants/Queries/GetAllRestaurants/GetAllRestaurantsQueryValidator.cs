8a88dab9fc95539aeadad5baa0a5feea87471755
Sinan <sinan.cakal00@gmail.com>
2024-07-07 22:18:08 +0300

    Validation for pagination

---
+++ PlateRate.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryValidator.cs
@@ -0,0 +1,19 @@
+﻿// Ignore Spelling: Validator
+
+using FluentValidation;
+
+namespace PlateRate.Application.Restaurants.Queries.GetAllRestaurants;
+public class GetAllRestaurantsQueryValidator : AbstractValidator<GetAllRestaurantsQuery>
+{
+    private int[] allowedPageSizes = [8,16,32];
+
+    public GetAllRestaurantsQueryValidator()
+    {
+        RuleFor(r => r.Page)
+            .GreaterThan(0);
+
+        RuleFor(r => r.Count)
+            .Must(value => allowedPageSizes.Contains(value))
+            .WithMessage($"Page size must be one of [{string.Join(",",allowedPageSizes)}]");
+    }
+}
