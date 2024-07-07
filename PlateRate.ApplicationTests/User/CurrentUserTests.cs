ac28887ff68dd3e0423f38e51dfa95b3baee4c8f
Sinan <sinan.cakal00@gmail.com>
2024-07-08 00:56:52 +0300

    Unit test for currentUser

---
+++ PlateRate.ApplicationTests/User/CurrentUserTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using PlateRate.Domain.Constants;
+using Xunit;
+
+namespace PlateRate.Application.User.Tests;
+
+public class CurrentUserTests
+{
+    // TestMethod_Scenario_ExpectedResult
+    [Theory()]
+    [InlineData(UserRoles.Admin)]
+    [InlineData(UserRoles.User)]
+    public void IsInRole_WithMatchingRole_ShouldReturnsTrue(string roleName)
+    {
+        // arrange
+        var currentUser = new CurrentUser("1", "test@test.com", [UserRoles.Admin,UserRoles.User]);
+
+        // act
+        var isInRole = currentUser.IsInRole(roleName);
+
+        // assert
+        isInRole.Should().BeTrue();
+    }
+
+    [Fact()]
+    public void IsInRole_WithNoMatchingRole_ShouldReturnsFalse()
+    {
+        // arrange
+        var currentUser = new CurrentUser("1", "test@test.com", [UserRoles.Admin, UserRoles.User]);
+
+        // act
+        var isInRole = currentUser.IsInRole(UserRoles.Owner);
+
+        // assert
+        isInRole.Should().BeFalse();
+    }
+
+}
