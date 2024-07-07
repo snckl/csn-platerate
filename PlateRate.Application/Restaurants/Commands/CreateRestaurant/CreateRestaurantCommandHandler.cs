66e55d187150979bc6946309a4788a635f2dd243
Sinan <sinan.cakal00@gmail.com>
2024-07-07 18:58:27 +0300

    minor changes on createRestaurantCommandHandler for uwner

--- PlateRate.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
+++ PlateRate.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
@@ -1,17 +1,21 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using PlateRate.Application.User;
 using PlateRate.Domain.Entities;
 using PlateRate.Domain.Repositories;
 
 namespace PlateRate.Application.Restaurants.Commands.CreateRestaurant;
 public class CreateRestaurantCommandHandler(ILogger<CreateRestaurantCommandHandler> logger
-    ,IMapper mapper,IRestaurantRepository restaurantRepository) : IRequestHandler<CreateRestaurantCommand, int>
+    ,IMapper mapper,IRestaurantRepository restaurantRepository,, IUserContext userContext) : IRequestHandler<CreateRestaurantCommand, int>
 {
     public async Task<int> Handle(CreateRestaurantCommand request, CancellationToken cancellationToken)
     {
-        logger.LogInformation("Creating restaurant {@Restaurant}",request);
+        var currentUser = userContext.GetCurrentUser();
+        logger.LogInformation("{Username} [{UserId}] Creating restaurant {@Restaurant}", currentUser.Email, currentUser.Id,request);
+
         var restaurant = mapper.Map<Restaurant>(request);
+        restaurant.OwnerId = currentUser.Id;
 
         int id = await restaurantRepository.CreateAsync(restaurant);
         return id;
